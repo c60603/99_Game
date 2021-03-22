7bd48da4f6c460c6b752433c8dbf08453a9b3066
jacksonLai59 <jiechun.lai@gmail.com>
2021-03-22 15:48:12 +0800

    Initial Commit

---
+++ 99_Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  99_Game
+//
+//  Created by User07 on 2021/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 99_Game/_9_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_GameApp.swift
+//  99_Game
+//
+//  Created by User07 on 2021/3/22.
+//
+
+import SwiftUI
+
+@main
+struct _9_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
